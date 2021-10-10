d4ca750dadcb188a1c9e649fe014eeaa8d8f549d
Siddharth Singh <simsid66@gmail.com>
2021-10-10 17:18:43 +0530

    create config.ts

---
+++ static/src/config.ts
@@ -0,0 +1,31 @@
+import { createDir, writeFile } from "@tauri-apps/api/fs";
+import { dirname } from "@tauri-apps/api/path";
+import { exit } from "@tauri-apps/api/process";
+import { editorConfirm, editorAlert, editorPrompt } from "./prompt";
+import { exists } from "./utils";
+
+export const createConfig = async (configPath): Promise<string> => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const confirm = await editorConfirm('Config file not found. Would you like to create one?');
+
+            if (!confirm) await editorAlert('Exiting...', () => exit(1));
+
+            const tmp: Record<string, any> = {
+                font: await editorPrompt('Pick a font.')
+            };
+
+            const dir = await dirname(configPath);
+            if (!(await exists(dir))) await createDir(dir, { recursive: true })
+
+            const string = JSON.stringify(tmp);
+            await writeFile({ contents: string, path: configPath })
+            await editorAlert(`Saved choices to ${configPath}`);
+            resolve(string);
+        }
+
+        catch (e) {
+            reject(e);
+        }
+    })
+}
--- static/src/main.ts
+++ static/src/main.ts
@@ -1,11 +1,9 @@
-import { createDir, readTextFile, writeFile } from '@tauri-apps/api/fs';
-import { dirname } from '@tauri-apps/api/path';
+import { readTextFile } from '@tauri-apps/api/fs';
 import cf from 'campfire.js';
 import CodeMirror from 'codemirror';
 import 'codemirror/mode/gfm/gfm';
-import { getConfigDir, rustLog, exists, getConfigPath } from './utils';
-import { exit } from '@tauri-apps/api/process';
-import { editorAlert, editorPrompt, editorChoose, editorConfirm } from './prompt';
+import { createConfig } from './config';
+import { getConfigPath } from './utils';
 
 // TODO: implement surrounding code
 const parseKeybind = (keybind: string, e: KeyboardEvent) => {
@@ -20,32 +18,6 @@ const parseKeybind = (keybind: string, e: KeyboardEvent) => {
     throw new Error("Not implemented.");
 
     return (toCheck.every((bool) => !!bool) && e.key.toLocaleLowerCase() == alpha);
-}
-
-const createConfig = async (configPath): Promise<string> => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const confirm = await editorConfirm('Config file not found. Would you like to create one?');
-
-            if (!confirm) await editorAlert('Exiting...', () => exit(1));
-
-            const tmp: Record<string, any> = {
-                font: await editorPrompt('Pick a font.')
-            };
-
-            const dir = await dirname(configPath);
-            if (!(await exists(dir))) await createDir(dir, { recursive: true })
-
-            const string = JSON.stringify(tmp);
-            await writeFile({ contents: string, path: configPath })
-            await editorAlert(`Saved choices to ${configPath}`);
-            resolve(string);
-        }
-
-        catch (e) {
-            reject(e);
-        }
-    })
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
