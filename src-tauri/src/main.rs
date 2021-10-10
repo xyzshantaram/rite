53dceee6d376bb4db3d63161f005c315131590b3
Siddharth Singh <simsid66@gmail.com>
2021-10-10 15:10:52 +0530

    get correct directory from get_config_dir

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,17 +5,31 @@
 
 #[tauri::command]
 fn get_config_dir() -> std::path::PathBuf {
-  let key = "RITE_DATA_DIR";
+  let key = "RITE_DIR";
 
-  return match std::env::var_os(key) {
-      Some(val) => val.into(),
-      None => dirs::config_dir().unwrap_or_default()
-  };
+  let mut default_path = dirs::config_dir().unwrap_or_default();
+  default_path.push("rite");
+
+  match std::env::var_os(key) {
+      Some(val) => {
+        let mut path = std::path::PathBuf::from(val);
+        path.push("config");
+        path
+      }
+      None => default_path
+  }
 }
+
+#[tauri::command]
+fn log(line: String) {
+  println!("{}", line);
+}
+
 
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![get_config_dir])
+    .invoke_handler(tauri::generate_handler![log])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
