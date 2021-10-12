4fb7f5019dfc7b485d422493ce362a971a3543a4
Siddharth Singh <simsid66@gmail.com>
2021-10-12 11:53:07 +0530

    add esbuild watcher, version bump

---
+++ esbuild.watcher.js
@@ -0,0 +1,10 @@
+require('esbuild').build({
+    entryPoints: ['static/src/main.ts'],
+    bundle: true,
+    outfile: 'static/js/main.js',
+    format: 'esm',
+    watch: true
+}).catch((err) => {
+    console.error(err)
+    process.exit(1)
+})
